79b09859dc0399fad081f26e06849e04d75a1a5b
GIHAA <gihansudeeparandimal@gmail.com>
2024-01-08 18:26:21 +0530

    Feat: proxy

--- backend/authService/src/utils/messageBroker.js
+++ backend/authService/src/utils/messageBroker.js
@@ -1,29 +1,30 @@
 import { moduleLogger } from '@sliit-foss/module-logger';
-import { v4 as uuidv4 } from 'uuid';
 import { RABBIMQ_CONFIG } from './config';
 const amqp = require('amqplib');
 
 const logger = moduleLogger('RabbitMQ');
+let connection;
 
 export async function consumeUserValidationMessages() {
   try {
-    const connection = await amqp.connect(RABBIMQ_CONFIG.URL);
+    connection = await connectToRabbitMQ();
     const channel = await connection.createChannel();
     const queue = RABBIMQ_CONFIG.USER_VALIDATION_QUEUE || 'user_validation_queue';
 
+    logger.info('RabbitMQ connection successful');
     // Ensure the queue exists before consuming
     await channel.assertQueue(queue, { durable: true });
 
     channel.consume(queue, (message) => {
       try {
         const userDetails = JSON.parse(message.content.toString());
-    
+
         logger.info(`Received message from User Service: ${JSON.stringify(userDetails)}`);
         // Validate user logic here
         const userValidated = validateUser(userDetails);
 
         logger.info(`Sending validation response: ${JSON.stringify(userValidated)}`);
-        
+
         // Respond to the Product Service
         channel.sendToQueue(message.properties.replyTo, Buffer.from(JSON.stringify({ userValidated })), {
           correlationId: message.properties.correlationId
@@ -36,12 +37,21 @@ export async function consumeUserValidationMessages() {
       }
     });
   } catch (error) {
-    console.error('Error setting up RabbitMQ connection:', error);
-    process.exit(1); // Exit the application if RabbitMQ setup fails
+    logger.error('Error setting up RabbitMQ connection:', error);
+    // Attempt to reconnect after a delay
+    setTimeout(consumeUserValidationMessages, 5000); // Reconnect after 5 seconds
   }
 }
 
-const  validateUser = (userDetails) => {
+const connectToRabbitMQ = async () => {
+  try {
+    return await amqp.connect(RABBIMQ_CONFIG.URL);
+  } catch (error) {
+    throw new Error(`Failed to connect to RabbitMQ: ${error.message}`);
+  }
+};
+
+const validateUser = (userDetails) => {
   // Replace this with your actual validation logic
   // Here, we preserve the original validation status
   const newUserDetails = { ...userDetails, valid: true };
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.12-management
+
+docker-compose build ; docker-compose up
