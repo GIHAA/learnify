791d650fdaa98a70e5bda851a370441a772a789c
GIHAA <gihansudeeparandimal@gmail.com>
2024-04-08 14:24:14 +0530

    Feat: init frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <div>
       <h1>Login</h1>
+      <Link to="/admin">Admin</Link>
+      <Link to="/home">Home</Link>
     </div>
   );
 };
