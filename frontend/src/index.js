7e5c11b7363de516d256c62c827ee2d6f1a7911d
GIHAA <gihansudeeparandimal@gmail.com>
2024-04-13 00:16:45 +0530

    init fe

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,31 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./App";
-import "./index.css";
+import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+// third party
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+// project imports
+import * as serviceWorker from 'serviceWorker';
+import App from 'App';
+import { store } from 'store';
+
+// style + assets
+import 'assets/scss/style.scss';
+import config from './config';
+
+// ==============================|| REACT DOM RENDER  ||============================== //
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <BrowserRouter basename={config.basename}>
       <App />
-    </Provider>
-  </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
