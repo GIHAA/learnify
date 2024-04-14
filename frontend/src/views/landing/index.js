1f2599fc0d572d0e7cd747f1743fcbf4281d1c68
GIHAA <gihansudeeparandimal@gmail.com>
2024-04-14 20:45:10 +0530

    wip

--- frontend/src/views/landing/index.js
+++ frontend/src/views/landing/index.js
@@ -1,15 +1,21 @@
 import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
-    return (
-      <div className="text-[30px] margin-[10px]">
-        landing page
-        <Button variant="contained" href="/pages/register/register3" color="primary">Sign in</Button> {/* Changed 'to' to 'href' */}
-        <Button variant="contained" href="/pages/login/login3" color="primary">Sign up</Button>
-        <Button variant="contained" href="/admin" color="primary">Admin</Button>
-      </div>
-    );
-  };
-  
+  return (
+    <div className="text-[30px] margin-[10px]">
+      landing page
+      <Link to="/pages/register/register3">
+        <Button variant="contained" color="primary">Sign in</Button>
+      </Link>
+      <Link to="/pages/login/login3">
+        <Button variant="contained" color="primary">Sign up</Button>
+      </Link>
+      <Link to="/admin">
+        <Button variant="contained" color="primary">Admin</Button>
+      </Link>
+    </div>
+  );
+};
 
 export default Landing;
