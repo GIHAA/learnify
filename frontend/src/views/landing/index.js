29a5c087fc3eef65047349bf0345c4ebee462d53
GIHAA <gihansudeeparandimal@gmail.com>
2024-04-14 21:18:42 +0530

    wip

--- frontend/src/views/landing/index.js
+++ frontend/src/views/landing/index.js
@@ -5,15 +5,15 @@ const Landing = () => {
   return (
     <div className="text-[30px] margin-[10px]">
       landing page
-      <Link to="/pages/register/register3">
-        <Button variant="contained" color="primary">Sign in</Button>
-      </Link>
-      <Link to="/pages/login/login3">
-        <Button variant="contained" color="primary">Sign up</Button>
-      </Link>
-      <Link to="/admin">
-        <Button variant="contained" color="primary">Admin</Button>
-      </Link>
+
+        <Button component={Link} to="/pages/register/register3" variant="contained" color="primary">Sign in</Button>
+
+
+        <Button component={Link} to="/pages/login/login3" variant="contained" color="primary">Sign up</Button>
+
+
+        <Button component={Link} to="/admin" variant="contained" color="primary">Admin</Button>
+
     </div>
   );
 };
