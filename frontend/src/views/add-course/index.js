3c4031aed6b17b702286292ab69b6cc2ea77ec4f
GIHAA <gihansudeeparandimal@gmail.com>
2024-04-14 21:31:46 +0530

    wip

--- frontend/src/views/add-course/index.js
+++ frontend/src/views/add-course/index.js
@@ -1,12 +1,18 @@
-// material-ui
-// project imports
 import MainCard from 'ui-component/cards/MainCard';
-
-// ==============================|| SAMPLE PAGE ||============================== //
+import Grid from '@mui/material/Grid';
 
 const SamplePage = () => (
   <MainCard title="Add Course">
-
+    <Grid container spacing={2}>
+      {/* Left Column */}
+      <Grid item xs={12} md={8}>
+        dynamic content goes here
+      </Grid>
+      {/* Right Column */}
+      <Grid item xs={12} md={4}>
+       list of lessions goes here
+      </Grid>
+    </Grid>
   </MainCard>
 );
 
