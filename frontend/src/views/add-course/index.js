987ad77ccc175dbf92087f0b5965a79596e24111
GIHAA <gihansudeeparandimal@gmail.com>
2024-04-15 15:46:50 +0530

    wip

--- frontend/src/views/add-course/index.js
+++ frontend/src/views/add-course/index.js
@@ -2,9 +2,9 @@ import MainCard from "ui-component/cards/MainCard";
 import LessionCard from "ui-component/cards/LessionCard";
 import Grid from "@mui/material/Grid";
 import { useState } from "react";
-import { Button, TextField, Avatar } from "@mui/material";
+import { Button, TextField, Avatar, CardMedia } from "@mui/material";
 // import ReactPlayer from "react-player/lazy";
-import { VideoPlayer } from "@graphland/react-video-player";
+// import { VideoPlayer } from "@graphland/react-video-player";
 import InputFileUpload from "ui-component/form-components/InputFileUpload";
 import LinearWithValueLabel from "ui-component/LinearProgressWithLabel";
 import AssignmentIcon from "@mui/icons-material/Assignment";
@@ -43,16 +43,16 @@ const AddCourse = () => {
 
   const videoSources = [
     {
-      src: "https://media.w3.org/2010/05/sintel/trailer_hd.mp4",
+      src: "https://firebasestorage.googleapis.com/v0/b/ds-project-53aa8.appspot.com/o/videos%2Fgojo2.mp4?alt=media&token=f7787d9f-b982-4725-9e53-d28d9d972dd5",
       type: "video/mp4",
     },
     // Add more video sources as needed
   ];
 
   const videoProps = {
-    theme: "city", // 'city', 'fantasy', 'forest', 'sea'
-    height: 720,
-    width: 1280,
+    theme: "forest", // 'city', 'fantasy', 'forest', 'sea'
+    height: "auto",
+    width: "auto",
     autoPlay: false,
     loop: false,
     sources: videoSources,
@@ -82,7 +82,16 @@ const AddCourse = () => {
               width="100%"
               height="auto"
             /> */}
-            <VideoPlayer {...videoProps} />
+            {/* <VideoPlayer {...videoProps} /> */}
+
+            <CardMedia 
+              component="video"
+              controls
+              src="https://firebasestorage.googleapis.com/v0/b/ds-project-53aa8.appspot.com/o/videos%2Fgojo2.mp4?alt=media&token=f7787d9f-b982-4725-9e53-d28d9d972dd5"
+              alt="green iguana"
+              title="green iguana"
+              className="w-full h-[300px] rounded-[7px]"
+            />
           </div>
           <div className="ml-2">
             <LinearWithValueLabel />
