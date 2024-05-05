bfd56a8584d68fa8449b951dac2369162fb621ff
Sachini Tharuka <sachinitkandambige29@gmail.com>
2024-05-05 15:22:59 +0530

    my courses

---
+++ frontend/src/views/my-courses/Completed/index.jsx
@@ -0,0 +1,62 @@
+import { Pagination } from "@mui/material";
+import { useEffect, useState } from "react";
+import { getCourses } from "api/courseService";
+import CourseCard from "ui-component/cards/CourseCard";
+
+const CompletedCourses = () => {
+  const [courses, setCourses] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const fetchCourses = async (page = 1, searchText = "") => {
+    try {
+      const response = await getCourses(page, 3, searchText);
+      setCourses(response.docs);
+      setTotalPages(response.totalPages);
+      setCurrentPage(page);
+    } catch (error) {
+      console.error("Error fetching courses:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
+  const handlePageChange = (event, page) => {
+    fetchCourses(page);
+  };
+
+  return (
+    <section className="bg-white rounded-[12px]">
+      <section className="lg:max-w-[1440px] max-w-[375px] md:max-w-[688px] mx-auto  px-[20px] py-[40px] hero-content mb-4">
+        <h2 className="text-[16px] leading-[24px] md:text-[24px] md:leading-[36px] font-medium pb-[30px]">
+         Completed Courses
+        </h2>
+        <div className="flex gap-[20px] flex-wrap max-w-[1280px] mx-auto justify-center">
+        {courses.map((course) => (
+          <CourseCard
+            key={course.id}
+            title={course.title}
+            description={course.description}
+            price={course.price}
+            image={course.thumbnail}
+          />
+        ))}
+        </div>
+       
+
+        <div className="mt-[40px] flex justify-center">
+          <Pagination
+            count={totalPages}
+            color="primary"
+            page={currentPage}
+            onChange={handlePageChange}
+          />
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default CompletedCourses;
--- frontend/src/views/my-courses/index.jsx
+++ frontend/src/views/my-courses/index.jsx
@@ -1,9 +1,14 @@
-
+import AllCourses from "./All";
+import CompletedCourses from "./Completed";
+import OngoingCourses from "./Incompleted";
 
 const MyCourse = () => {
   return (
-    <div>
-      Hello
+    <div className="flex flex-col">
+     <h1 className=" text-[32px] leading-[40px] md:text-[40px] md:leading-[46px] font-bold mb-[30px]">My Courses</h1>
+     <AllCourses/>
+     <OngoingCourses/>
+     <CompletedCourses/>
     </div>
   );
 };
